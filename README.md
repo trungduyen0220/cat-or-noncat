26fbceb1c77f9929986b1b3177b36e696ae8021b
ntmduyen <ntmduyen@cmc.com.vn>
2020-08-14 12:51:56 +0700

    update different initialization methods

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# cat-or-noncat
+# detect cat
+
+detect_cat_v1.ipy
 - Work with logistic regression in a way that builds intuition relevant to neural networks.
 - Learn how to minimize the cost function.
 - Understand how derivatives of the cost are used to update parameters.
 
+detect_cat_v2.ipy
+- Change the different types of initializations
 references: Neural Network and Deep Learning - Coursera course
 https://www.coursera.org/learn/neural-networks-deep-learning
--- lr_utils.py
+++ lr_utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import h5py
-    
+import sklearn
+import sklearn.datasets
+import matplotlib.pyplot as plt
     
 def load_dataset():
     train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
@@ -12,8 +14,21 @@ def load_dataset():
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+
+    train_set_y = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    train_set_x = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+
+    train_X, train_Y = sklearn.datasets.make_circles(n_samples=300, noise=.05)
+    test_X, test_Y = sklearn.datasets.make_circles(n_samples=100, noise=.05)
+    # Visualize the data
+    plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+    train_X = train_X.T
+    train_Y = train_Y.reshape((1, train_Y.shape[0]))
+    test_X = test_X.T
+    test_Y = test_Y.reshape((1, test_Y.shape[0]))
     
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    return train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes
